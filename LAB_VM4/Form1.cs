a9e7ac6553bfcfec7c6e05c88b53b3f10f7c151b
Luzyanin Alexey <261-96@mail.ru>
2018-04-18 23:25:37 +0700

    интерфейс

--- LAB_VM4/Form1.cs
+++ LAB_VM4/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace LAB_VM4
 {
@@ -15,6 +16,34 @@ namespace LAB_VM4
         public Form1()
         {
             InitializeComponent();
+        }
+
+        int N = 0;
+
+        private void DerivativeButton_Click(object sender, EventArgs e)
+        {
+            decimal[,] input = new decimal[2, N];          
+
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < N; j++) input[i, j] = Convert.ToDecimal(dataGridView1[j, i].Value);
+            }
+
+            Series series = new Series();
+            
+            foreach (var result in Derivative.FindDerivative(input))
+            {
+                series.Points.AddXY(result.x, result.y);
+            }
+
+            chart1.Series.Add(series);
+        }
+
+        private void EnterButton_Click(object sender, EventArgs e)
+        {
+            N = Convert.ToInt32(NTextBox.Text);
+            dataGridView1.ColumnCount = N;
+            dataGridView1.RowCount = 2;
         }
     }
 }
