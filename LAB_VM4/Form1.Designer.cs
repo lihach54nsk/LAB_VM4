38d298b1dbd72a1f06943a3ed6b1998d9ba36218
Luzyanin Alexey <261-96@mail.ru>
2018-04-19 09:31:04 +0700

    Интерфейс

--- LAB_VM4/Form1.Designer.cs
+++ LAB_VM4/Form1.Designer.cs
@@ -109,10 +109,12 @@
             this.chart1.ChartAreas.Add(chartArea1);
             legend1.Name = "Legend1";
             this.chart1.Legends.Add(legend1);
-            this.chart1.Location = new System.Drawing.Point(409, 150);
+            this.chart1.Location = new System.Drawing.Point(12, 150);
             this.chart1.Name = "chart1";
             series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
             series1.Legend = "Legend1";
+            series1.MarkerSize = 1;
             series1.Name = "Series1";
             this.chart1.Series.Add(series1);
             this.chart1.Size = new System.Drawing.Size(378, 340);
